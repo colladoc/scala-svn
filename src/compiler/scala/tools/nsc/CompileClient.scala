e0e5caa710c7adfe36fa8bc9329a5c3d9c93646c
fsalvi <user@domain.org>
2011-04-26 08:27:36 +0000

    Add new branch for Colladoc (scala 2.9)

---
+++ src/compiler/scala/tools/nsc/CompileClient.scala
@@ -0,0 +1,68 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2011 LAMP/EPFL
+ * @author  Martin Odersky
+ */
+
+package scala.tools.nsc
+
+import java.io.{ BufferedReader, File, InputStreamReader, PrintWriter }
+import settings.FscSettings
+import scala.tools.util.CompileOutputCommon
+import sys.SystemProperties.preferIPv4Stack
+
+/** The client part of the fsc offline compiler.  Instead of compiling
+ *  things itself, it send requests to a CompileServer.
+ */
+class StandardCompileClient extends HasCompileSocket with CompileOutputCommon {
+  lazy val compileSocket: CompileSocket = CompileSocket
+
+  val versionMsg = "Fast " + Properties.versionMsg
+  var verbose    = false
+
+  def process(args: Array[String]): Int = {
+    // Trying to get out in front of the log messages in case we're
+    // going from verbose to not verbose.
+    verbose = (args contains "-verbose")
+    
+    val settings     = new FscSettings(Console.println)
+    val command      = new OfflineCompilerCommand(args.toList, settings)
+    val shutdown     = settings.shutdown.value
+    val extraVmArgs  = if (settings.preferIPv4.value) List("-D%s=true".format(preferIPv4Stack.key)) else Nil
+
+    val vmArgs  = settings.jvmargs.unparse ++ settings.defines.unparse ++ extraVmArgs
+    val fscArgs = args.toList ++ command.extraFscArgs
+    
+    if (settings.version.value) {
+      Console println versionMsg
+      return 0
+    }
+
+    info(versionMsg)
+    info(args.mkString("[Given arguments: ", " ", "]"))
+    info(fscArgs.mkString("[Transformed arguments: ", " ", "]"))
+    info(vmArgs.mkString("[VM arguments: ", " ", "]"))
+
+    val socket =
+      if (settings.server.value == "") compileSocket.getOrCreateSocket(vmArgs mkString " ", !shutdown)
+      else Some(compileSocket.getSocket(settings.server.value))
+    
+    val success = socket match {
+      case Some(sock) => compileOnServer(sock, fscArgs)
+      case _          =>
+        echo(
+          if (shutdown) "[No compilation server running.]"
+          else "Compilation failed."
+        )
+        shutdown
+    }
+    if (success) 1 else 0
+  }
+}
+
+object CompileClient extends StandardCompileClient {
+  def main(args: Array[String]): Unit = sys exit {
+    try process(args)
+    catch { case _: Exception => 1 }
+  }
+}
+
